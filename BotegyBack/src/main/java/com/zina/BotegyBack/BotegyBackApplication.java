e0f3f78ed4575931e256ca2650dbe5fa38888cb5
saved2223 <shatal2001@yandex.ru>
2022-03-09 10:49:18 +0300

    initial commit

---
+++ BotegyBack/src/main/java/com/zina/BotegyBack/BotegyBackApplication.java
@@ -0,0 +1,13 @@
+package com.zina.BotegyBack;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotegyBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BotegyBackApplication.class, args);
+	}
+
+}
---
+++ BotegyBack/src/test/java/com/zina/BotegyBack/BotegyBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zina.BotegyBack;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotegyBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
